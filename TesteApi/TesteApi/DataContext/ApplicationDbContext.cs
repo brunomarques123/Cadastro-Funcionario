f8abf567ce14804f551ab0f071f099df891fc88e
Bruno Marques <b_marques85@live.com>
2024-10-14 16:04:51 -0300

    Adicionar arquivos de projeto.

---
+++ TesteApi/TesteApi/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TesteApi.Models;
+
+namespace TesteApi.DataContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) 
+            : base(options)
+        { 
+        }
+
+
+        public DbSet<FuncionarioModel> Funcionarios { get; set; }
+    }
+}
---
+++ TesteApi/TesteApi/Enums/TurnoEnum.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TesteApi.Enums
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum TurnoEnum
+    {
+        Manha,
+        Tarde,
+        Noite
+    }
+}
